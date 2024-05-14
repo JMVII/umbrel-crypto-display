40b3847019779b5ef69000d14a5eefa3be37d52f
Jan Monge <janmonge@Jans-MacBook-Pro.local>
2024-05-13 17:01:38 -0700

    Changed output setting to DSI display

--- configure.sh
+++ configure.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-CRYPTOS="cryptos=["
-echo "Enter the cryptocurrency tickers you want to show separated by commas (ex: btc, eth, ada)"
-IFS=',' read -r -a array
-for index in "${!array[@]}"
-do
-    if (( $index != 0 )) ; then
-    	CRYPTOS="${CRYPTOS}, "
-    fi
-    CRYPTOS="${CRYPTOS}\""
-    CRYPTOS="${CRYPTOS}"${array[index]//[[:blank:]]/}
-    CRYPTOS="${CRYPTOS}\""
-done
-CRYPTOS="${CRYPTOS}]"
-sed -i "s/cryptos=.*/$CRYPTOS/g" /home/umbrel/umbrel-crypto-display/scripts/cryptoprice.py
-echo "To see the new cryptocurrencies you need to restart the cryptoprice script. Do you want do do it now?"
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+# Install fbi package for image display
+apt-get -y install fbi
+
+# Remove or comment out GPIO screen configuration from /boot/config.txt
+sed -i '/dtoverlay=piscreen/d' /boot/config.txt
+sed -i '/dtparam=spi=on/d' /boot/config.txt
+
+# Make sure spi-bcm2835 is not loaded
+rmmod spi-bcm2835
+
+# Make all .sh files executable
+chmod +x *.sh
+
+# Prompt the user to reboot the Raspberry Pi to apply the changes
+echo "To see the screen, you need to reboot your Raspberry Pi. Do you want to do it now?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) bash stop.sh; bash start.sh; break;;
+        Yes ) ./umbrel/scripts/stop; reboot; break;;
         No ) exit;;
     esac
 done
